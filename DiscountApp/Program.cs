c5f3a11b0f7bda1b8413df62a23be1512b2829c0
Theophilus Unekwu <theophilusu@examroom.ai>
2024-05-24 00:17:17 +0100

    quick fix

--- DiscountApp/Program.cs
+++ DiscountApp/Program.cs
@@ -1,5 +1,4 @@
 
-using Core.DiscountServices;
 using Data.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,8 +15,8 @@ var app = builder.Build();
 
 
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<DiscountService>();
+//// Configure the HTTP request pipeline.
+//app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
